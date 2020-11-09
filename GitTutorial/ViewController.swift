3123948aa3cde1b42366b52974419c1192f3a98f
Jawaher Alkhathlan <201700732@student.polytechnic.bh>
2020-11-09 15:05:27 +0300

    added message and print it

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    let message = "Hello Git!"
+    
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           print(message)
+       }
+    
+       override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+           // Dispose of any resources that can be recreated.
+       }
 
 }
 
