703edfe1748b04969ae2b7e76f1d57f129744f5f
Jawaher Alkhathlan <201700732@student.polytechnic.bh>
2020-11-09 15:11:37 +0300

    reverse method has been added

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
     let message = "Hello Git!"
     
        override func viewDidLoad() {
-           super.viewDidLoad()
-           print(message)
+            super.viewDidLoad()
+            print(message)
+            print(reverse(text: "stressed"))
        }
+    
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
     
        override func didReceiveMemoryWarning() {
            super.didReceiveMemoryWarning()
