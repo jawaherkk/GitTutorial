d5b89386b00d0aeacfcdb50698283d42943ea0a5
Jawaher Alkhathlan <201700732@student.polytechnic.bh>
2020-11-09 14:58:44 +0300

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Jawaher AlKhathlan on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
